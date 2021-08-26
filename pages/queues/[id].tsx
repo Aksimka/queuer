f564bb09a34c30cadafe73e33c1f3482a19660f4
Maksim Safonov <msafonov@metaprime.ru>
2021-08-27 00:15:29 +0300

    begin queue page

---
+++ pages/queues/[id].tsx
@@ -0,0 +1,17 @@
+import { ReactElement } from 'react'
+import { useRouter } from 'next/router'
+import MenuLayout from '@/layouts/menu'
+import MainLayout from '@/layouts/main'
+
+export default function Queue(): ReactElement {
+  const router = useRouter()
+  console.log(router, 'router')
+  const { id } = router.query
+  return (
+    <MenuLayout>
+      <MainLayout heading={`${id}`} footer="footer">
+        <div>{id}</div>
+      </MainLayout>
+    </MenuLayout>
+  )
+}
