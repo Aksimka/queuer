b20f19e50f31ac7338899a637f0ac3e290051c98
Maksim Safonov <mks_comp97@mail.ru>
2021-09-09 00:46:33 +0300

    app interfaces fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,23 @@
 import '../styles/reset.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { NextComponentType } from "next/dist/next-server/lib/utils";
-import Modify from "../types/modify";
-import { ReactElement } from "react";
+import { NextComponentType } from 'next/dist/next-server/lib/utils'
+import Modify from '../types/modify'
+import { ReactElement } from 'react'
 
-type ExtendedNuxtPage = { withLayout: Function }
+type ExtendedNuxtPage = {
+  withLayout(comp: ReactElement): ReactElement
+}
 
 type ExtendedComponentType = NextComponentType & ExtendedNuxtPage
 
-interface ExtendedComponent{
+interface ExtendedComponent {
   Component: ExtendedComponentType
 }
 
-interface ExtendedAppProps extends Modify<AppProps, ExtendedComponent>{}
+type ExtendedAppProps = Modify<AppProps, ExtendedComponent>
 
-function MyApp({ Component, pageProps }: ExtendedAppProps) {
+function MyApp({ Component, pageProps }: ExtendedAppProps): ReactElement {
   const withLayout = Component.withLayout || ((page: ReactElement) => page)
 
   return withLayout(<Component {...pageProps} />)
