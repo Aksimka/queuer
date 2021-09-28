7c71bfcc88c0ce961de411c41973b90e4a00bf4c
Maksim Safonov <mks_comp97@mail.ru>
2021-09-29 00:51:31 +0300

    media queries

--- blocks/queue/Images/Images.tsx
+++ blocks/queue/Images/Images.tsx
@@ -1,7 +1,5 @@
 import React, { FC, ReactElement, useEffect, useState } from 'react'
 import classes from './Images.module.css'
-import Carousel, { CarouselImagesProp } from '@/components/ui/Carousel/Carousel'
-import imagesArray from './imagesArray'
 import classNames from 'classnames'
 
 type ImageProp = {
@@ -15,22 +13,12 @@ type PropTypes = {
 
 const Images: FC<PropTypes> = (props): ReactElement => {
   const { images } = props
-  const onWindowResizedFunc = (): void => {
-    if (window.innerWidth <= 768) {
-      setOtherImgsWidth(60)
-    } else {
-      setOtherImgsWidth(120)
-    }
-  }
 
   const [mainImage, setMainImage] = useState('')
-  const [otherImgsWidth, setOtherImgsWidth] = useState(120)
 
   useEffect(() => {
-    setMainImage('/images/ps5.png')
-
-    window.onresize = onWindowResizedFunc
-  }, [])
+    setMainImage(images[0].path)
+  }, [images])
 
   const selectImage = (e: string) => {
     setMainImage(e)
@@ -39,18 +27,20 @@ const Images: FC<PropTypes> = (props): ReactElement => {
   return (
     <div className={classes.Images}>
       <div className={classes.ImagesMain}>
-        <div className={classNames([classes.BigImage, 'standard-shadow'])}>
-          <img
-            src={mainImage}
-            alt="Изображение товара"
-            className={classes.BigImage}
-          />
-        </div>
+        <img
+          src={mainImage}
+          alt="Изображение товара"
+          className={classNames([classes.BigImage, 'standard-shadow'])}
+        />
       </div>
       <div className={classes.OtherImages}>
         {images.map((item) => {
           return (
-            <div className={classes.OtherImage} key={item.id}>
+            <div
+              className={classes.OtherImage}
+              key={item.id}
+              onClick={() => selectImage(item.path)}
+            >
               <img src={item.path} alt="Изображение товара" />
             </div>
           )
